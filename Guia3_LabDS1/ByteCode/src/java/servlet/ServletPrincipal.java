b233d911cb595980fe3c417113645237213130e2
Fernández Zavaleta <fz21i04001@usonsonate.edu.sv>
2023-09-21 16:28:09 -0300

    Entrega Guia 3

---
+++ Guia3_LabDS1/ByteCode/src/java/servlet/ServletPrincipal.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ServletPrincipal extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletPrincipal</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletPrincipal at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         String accion = request.getParameter("accion");
+        if (accion == null) {
+            request.getRequestDispatcher("/index.html").forward(request, response);
+        } else if (accion.equals("Login")) {
+            request.getRequestDispatcher("/Login.html").forward(request, response);
+        } else if (accion.equals("RegistroEmpleado")) {
+            request.getRequestDispatcher("/RegistroEmpleado.html").forward(request, response);
+        } else if (accion.equals("RegistroProducto")) {
+            request.getRequestDispatcher("/RegistroProducto.html").forward(request, response);
+        } else if (accion.equals("RegistroCliente")) {
+            request.getRequestDispatcher("/RegistroCliente.html").forward(request, response);
+        } else if (accion.equals("RegistroVenta")) {
+            request.getRequestDispatcher("/RegistroVenta.html").forward(request, response);
+        }
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
